b42d484a88a745858b16b41bd32b581d73a81b7d
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-05-03 17:27:19 +0000

    added half code

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <DOCTYPE html>
+<html lang="en-ca">
+  <head>
+
+    <!--Meta data-->
+    <meta charset="utf-8">
+    <meta name="description" content="Number Display webpage">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="Lily C">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/site.webmanifest">
+
+    <!--Stylesheet CSS-->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!--MDL theme-->
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-indigo.min.css" />
+
+    <!-- Title -->
+    <title>Number Display Webpage</title>
+  </head>
+  <body>
+
+    <!--Link to JS file-->
+    <script src="./js/script.js"></script>
+
+    <!--Header-->
+    <?php echo "<h1>Number Display Webpage, PHP</h1>"; ?>
